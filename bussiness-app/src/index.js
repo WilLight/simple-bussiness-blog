d00f75cf01f871818995259a7b17b9819609998f
Willight <willight.everfor@gmail.com>
2019-07-24 11:01:03 +0200

    Adding router to the project and restructuring

--- bussiness-app/src/index.js
+++ bussiness-app/src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DocTop from './Doc-top';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import  { Button } from '@material-ui/core';
+
+import About from './components/about';
+import Contact from './components/contact';
+import Newsfeed from './components/newsfeed';
+
+import logo from './logo.svg'
 import './index.css';
 //import * as serviceWorker from './serviceWorker';
+
+
+function DocTop() {
+    return (
+      <div id="Document-top-container">
+        <img src={logo} id="Company-logo" alt="logo" />
+        <p id="Document-top-text">
+          Company Name
+        </p>
+        <div className="Document-top-buttons">
+          <Link to = "/">
+            <Button
+  
+              className="Document-top-button"
+            >
+              Home
+            </Button>
+          </Link>
+          <Link to = "/newsfeed/">
+            <Button
+              className="Document-top-button"
+            >
+              Gallery
+            </Button>
+          </Link>
+          <Link to = "/contact/">
+            <Button
+              className="Document-top-button"
+            >
+              Contact Us
+            </Button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+
 class Bussiness extends React.Component {
     constructor() {
         super();
@@ -11,17 +56,26 @@ class Bussiness extends React.Component {
     render() {
         return(
             <div className="document">
+            <Router>
                 <div className="document-top">
                     <DocTop />
                 </div>
                 <div className="document-body">
+                
+                    <Route path="/" exact component={About} />
+                    <Route path="/newsfeed/" component={Newsfeed} />
+                    <Route path="/contact/" component={Contact} />
+                
                 </div>
+            </Router>
             </div>
         )
         
         
     }
 }
+
+
 
 ReactDOM.render( <Bussiness /> , document.getElementById('root'));
 
